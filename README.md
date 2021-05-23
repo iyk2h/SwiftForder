fbf1b958e5cf94fd16d41892439a2600fba496a1
youngkyunIm <yee0230@naver.com>
2021-05-23 15:18:49 +0900

    README.MD

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Swift
+# This forder is for Swift study
+
+### UpDownGame Forder is From yagom Youtube clone coding - https://youtu.be/aVpSUBlZPxU
---
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by Youngkyoon Im on 2021/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
