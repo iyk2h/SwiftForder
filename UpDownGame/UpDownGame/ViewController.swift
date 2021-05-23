17ed527bb99c23e523a348d40092857f92511ef4
youngkyunIm <yee0230@naver.com>
2021-05-23 17:59:19 +0900

    Lesson4

--- UpDownGame/UpDownGame/ViewController.swift
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func sliderValueChanged(_ sender:UISlider) {
+        print(sender.value)
+    }
+    
+    @IBAction func tpuchUpHitButton(_ sender:UIButton) {
+        print(slider.value)
+    }
+    
+    @IBAction func touchUpResetButton(_ send:UIButton) {
+        print("touch up reset button")
+        slider.value = 15
+    }
 }
 
