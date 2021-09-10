717c01dade073d88b3c4f4a8add4a0cf7a5704cb
youngkyunIm <yee0230@naver.com>
2021-09-10 20:07:04 +0900

    Lesson4

---
+++ UpDownGame/CreaditViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CreaditViewController.swift
+//  UpDownGame
+//
+//  Created by Youngkyoon Im on 2021/05/23.
+//
+
+import UIKit
+
+class CreaditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func touchUpDismissButton(_ sender: UIButton) {
+        dismiss(animated: true,
+                completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- UpDownGame/UpDownGame/ViewController.swift
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -8,25 +8,81 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var randomValue: Int = 0
+    var tryCount: Int = 0
+    
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tryCountLabel: UILabel!
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var minimumValueLabel: UILabel!
+    @IBOutlet weak var maximumValueLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        reset()
     }
     
     @IBAction func sliderValueChanged(_ sender:UISlider) {
         print(sender.value)
+        let integerValue: Int = Int(sender.value)
+        sliderValueLabel.text = String(integerValue)
+    }
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: nil,
+                                      message: message,
+                                      preferredStyle: .alert)
+        let okAcrion = UIAlertAction(title: "OK",
+                                     style: .default) { (action) in
+            self.reset()
+        }
+        alert.addAction(okAcrion)
+        present(alert,
+                animated: true,
+                completion: nil)
     }
     
     @IBAction func tpuchUpHitButton(_ sender:UIButton) {
         print(slider.value)
+        let hitValue: Int = Int(slider.value)
+        slider.value = Float(hitValue)
+        
+        tryCount += 1
+        tryCountLabel.text = "\(tryCount) / 5"
+
+        if randomValue == hitValue {
+            showAlert(message: "You Hit!")
+            reset()
+            return
+        } else if tryCount >= 5 {
+            showAlert(message: "You Loss!")
+            reset()
+            return
+        } else if randomValue > hitValue {
+            slider.minimumValue = Float(hitValue)
+            minimumValueLabel.text = String(hitValue)
+        } else {
+            slider.maximumValue = Float(hitValue)
+            maximumValueLabel.text = String(hitValue)
+        }
     }
     
     @IBAction func touchUpResetButton(_ send:UIButton) {
         print("touch up reset button")
+        reset()
+    }
+    
+    func reset() {
+        print("reset!")
+        randomValue = Int.random(in: 0...30)
+        tryCount = 0
+        tryCountLabel.text = "0 / 5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
         slider.value = 15
+        minimumValueLabel.text = "0"
+        maximumValueLabel.text = "30"
+        sliderValueLabel.text = "15"
     }
 }
-
